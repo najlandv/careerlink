eee29baf2e33df62590943cfae38fabe9a096bb8
Najla Nadiva <nadiva979@gmail.com>
2024-11-08 11:23:30 +0700

    pesan

--- app/src/main/java/com/example/careerlink/MainActivity.kt
+++ app/src/main/java/com/example/careerlink/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,28 +19,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             CareerlinkTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RegisterScreen(modifier = Modifier.padding(innerPadding)) // Meneruskan innerPadding ke RegisterScreen
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RegisterScreenPreview() {
     CareerlinkTheme {
-        Greeting("Android")
+        RegisterScreen()
     }
 }
