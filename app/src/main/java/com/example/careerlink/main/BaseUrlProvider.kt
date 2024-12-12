ec83b7b03e9f9a705f72d1c460e9fc66faf296f8
Najla Nadiva <nadiva979@gmail.com>
2024-12-12 12:05:56 +0700

    edit loker done

--- app/src/main/java/com/example/careerlink/main/BaseUrlProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.careerlink.main
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class BaseUrlProvider @Inject constructor() {
-    val baseUrl = "https://n6j4w26m-3000.asse.devtunnels.ms/"
-}
