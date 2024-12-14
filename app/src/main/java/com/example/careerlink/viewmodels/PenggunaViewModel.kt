2b618a9e3ea2f71530693aeeae680911500df0c0
Najla Humaira <najlahumairadesni@gmail.com>
2024-12-14 19:35:46 +0700

    ganti password

--- app/src/main/java/com/example/careerlink/viewmodels/PenggunaViewModel.kt
+++ app/src/main/java/com/example/careerlink/viewmodels/PenggunaViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.careerlink.viewmodels
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.careerlink.data.TokenDataStore
+import com.example.careerlink.models.ChangePasswordRequest
 import com.example.careerlink.models.PenggunaData
 import com.example.careerlink.services.PenggunaApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -28,7 +30,7 @@ class PenggunaViewModel @Inject constructor(
         getPengguna()
     }
 
-    fun getPengguna() {
+    private fun getPengguna() {
         viewModelScope.launch {
             val token = tokenDataStore.accessToken.firstOrNull()
             if (!token.isNullOrEmpty()) {
@@ -38,6 +40,48 @@ class PenggunaViewModel @Inject constructor(
                 } catch (e: Exception) {
                     e.printStackTrace()
                     _errorMessage.value = "Gagal mengambil data pengguna"
+                }
+            } else {
+                _errorMessage.value = "Token tidak ditemukan"
+            }
+        }
+    }
+
+    fun changePassword(
+        context: Context,
+        passwordLama: String,
+        passwordBaru: String,
+        konfirmasiPassword: String,
+        onSucces: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+        viewModelScope.launch {
+            val token = tokenDataStore.accessToken.firstOrNull()
+            if (!token.isNullOrEmpty()) {
+                try {
+                    // Validasi input sebelum mengirimkan permintaan
+                    if (passwordBaru != konfirmasiPassword) {
+                        _errorMessage.value = "Password baru dan konfirmasi password tidak cocok"
+                        return@launch
+                    }
+
+                    // Buat request body
+                    val request = ChangePasswordRequest(
+                        passwordLama = passwordLama,
+                        passwordBaru = passwordBaru,
+                        konfirmasiPassword = konfirmasiPassword
+                    )
+
+                    // Panggil API
+                    val response = penggunaApiService.changePassword("Bearer $token", request)
+                    if (response.isSuccessful) {
+                        onSucces
+                    } else {
+                        onError("Gagal mengupdate password: ${response.message()}")
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    onError("Terjadi kesalahan: ${e.message}")
                 }
             } else {
                 _errorMessage.value = "Token tidak ditemukan"
